bd48a5268e8e52ee85e2b4e411ad2fb8de501436
conrad <conrado5@gmx.de>
2020-06-17 00:54:43 +0200

    save

---
+++ src/app/warehouse/entity/LiquidBulkCargo.java
@@ -0,0 +1,36 @@
+package app.warehouse.entity;
+
+import storageContract.administration.Customer;
+import storageContract.cargo.Hazard;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.util.Collection;
+import java.util.Date;
+
+public class LiquidBulkCargo extends Item implements storageContract.cargo.LiquidBulkCargo {
+    private Boolean pressurized;
+
+    public LiquidBulkCargo(
+            BigDecimal weight,
+            Customer owner,
+            Collection<Hazard> hazards,
+            ZonedDateTime expireDate,
+            Boolean pressurized
+    ) {
+        super(weight, owner, hazards, expireDate);
+
+        this.type = "LiquidBulkCargo";
+        this.pressurized = pressurized;
+    }
+
+    @Override
+    public boolean isPressurized() {
+        return this.pressurized;
+    }
+
+    public void setPressurized(Boolean pressurized) {
+        this.pressurized = pressurized;
+    }
+}
---
+++ src/app/warehouse/entity/UnitisedCargo.java
@@ -0,0 +1,33 @@
+package app.warehouse.entity;
+
+import storageContract.administration.Customer;
+import storageContract.cargo.Hazard;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.util.Collection;
+import java.util.Date;
+
+public class UnitisedCargo extends Item implements storageContract.cargo.UnitisedCargo {
+
+    private Boolean fragile;
+
+    public UnitisedCargo(
+            BigDecimal weight,
+            Customer owner,
+            Collection<Hazard> hazards,
+            ZonedDateTime expireDate,
+            Boolean fragile
+    ) {
+        super( weight, owner, hazards, expireDate);
+        this.fragile = fragile;
+        this.type = "UnitisedCargo";
+    }
+
+    @Override
+    public boolean isFragile() {
+        return this.fragile;
+    }
+
+}
